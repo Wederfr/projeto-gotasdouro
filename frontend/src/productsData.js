eabbbe5a40a48a000f18fab2c99c2e784f56a76a
Wederfr <wederfr@hotmail.com>
2026-02-16 12:24:16 -0300

    atualização dos produtos

--- frontend/src/productsData.js
+++ frontend/src/productsData.js
@@ -3,31 +3,76 @@ const products = [
     id: 'prod001',
     name: 'Chuvisco cristalizado',
     description: 'O chuvisco crocante com cobertura de açúcar cristalizado.',
-    price: 15.00,
-    imageUrl: '/images/cristalizado.jpeg',
+    price: 13.00,
+    imageUrl: '/images/crist2.jpeg',
   },
-  {
+
+   {
     id: 'prod002',
-    name: 'Chuvisco em calda',
-    description: 'O clássico chuvisco em calda, preparado com a receita original.',
-    price: 18.00,
-    imageUrl: '/images/caldacom.jpeg',
+    name: 'Chuvisco cristalizado com nozes',
+    description: 'O chuvisco crocante com cobertura de açúcar cristalizado com nozes.',
+    price: 13.00,
+    imageUrl: '/images/prod cris.jfif',
   },
+  
   {
     id: 'prod003',
-    name: 'Chuvisco com Chocolate',
-    description: 'Delicioso chuvisco coberto com chocolate meio amargo.',
-    price: 22.00,
-    imageUrl: '/images/prod simp.jfif',
+    name: 'Chuvisco em calda',
+    description: 'O clássico chuvisco em calda em vidro 450g, Embalagem juta.',
+    price: 38.00,
+    imageUrl: '/images/calda emb.jpeg',
   },
 
   {
     id: 'prod004',
-    name: 'Copinho de Chuvisco em calda',
+    name: 'Chuvisco em calda',
+    description: 'O clássico chuvisco em calda em pote de 500g, preparado com a receita original.',
+    price: 28.00,
+    imageUrl: '/images/calda pote.jpeg',
+  },
+  
+
+  {
+    id: 'prod005',
+    name: 'Copinho de Chuvisco em calda 120g ',
     description: 'Delicioso chuvisco em Calda na medida certa de uma sobremesa deliciosa.',
-    price: 15.00,
+    price: 7.50,
     imageUrl: '/images/copo-chuvisco.jpeg',
   },
+
+  {
+    id: 'prod006',
+    name: 'Chuvisco em calda para presente',
+    description: 'Delicioso chuvisco em Calda em uma caixa personalizada. Ideal para presentear.',
+    price: 60.00,
+    imageUrl: '/images/calda cx2.jpeg',
+  },
+
+   {
+    id: 'prod007',
+    name: 'Conjunto com caneca para presente',
+    description: 'Delicioso chuvisco em Calda com uma linda caneca personalizada. Ideal para presentear.',
+    price: 70.00,
+    imageUrl: '/images/prod pres.jfif',
+  },
+
+  {
+    id: 'prod008',
+    name: 'Chuviscos para festas formato original',
+    description: 'Chuviscos banhados em chocolate, formato original decorados para festa. Mínimo 50 unidades',
+    price: 15.00,
+    imageUrl: '/images/crist nozes1.jpeg',
+  },
+
+  {
+    id: 'prod009',
+    name: 'Chuviscos para festas formato docinhos finos',
+    description: 'Chuviscos banhados em chocolate, formato docinhos finos para festa. Mínimo 50 unidades',
+    price: 15.00,
+    imageUrl: '/images/crist nozes3.jpeg',
+  },
+
+  
   
   // Adicionar mais produtos aqui, seguindo o mesmo formato
   
